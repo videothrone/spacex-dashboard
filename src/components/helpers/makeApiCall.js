9ba2c05ce4813e993c54940bc86965ee58a76c21
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-04-11 12:34:00 +0200

    ♻️ Add missing semicolon to makeApiCall

--- src/components/helpers/makeApiCall.js
+++ src/components/helpers/makeApiCall.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const makeApiCall = async (path) => {
   try {
     const {data: response} = await axios.get('https://api.spacexdata.com/v4/launches/' + path);
-    return response
+    return response;
   } catch (error) {
     console.error(error.message);
   }
