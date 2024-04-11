3f57fc80e861edd19155bebaa32d5a9baa1aeb2b
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-04-11 12:28:03 +0200

    ♻️ Replace Overlay icon /w FontAwesome

--- src/components/overlay/Overlay.js
+++ src/components/overlay/Overlay.js
@@ -1,10 +1,16 @@
 import './overlay.scss';
-import { copyIcon } from '../../assets/icons/icons.js';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Overlay = ({ message, className }) => {
   return (
     <div className='overlay' role='alert'>
-      <div className={`overlay-message ${className}`}>{message} <span className='overlay-icon' aria-hidden='true'>{copyIcon}</span></div>
+      <div className={`overlay-message ${className}`}>
+        {message}
+        <span className='overlay-icon' aria-hidden='true'>
+          <FontAwesomeIcon icon={faCopy} />
+        </span>
+      </div>
     </div>
   );
 };
