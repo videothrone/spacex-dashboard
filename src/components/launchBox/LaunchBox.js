e042b56ad245032a5f2a4e43a37b68bcaa273c65
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-04-11 12:30:32 +0200

    ♻️ Replace LaunchBox icon /w FontAwesome
    + Adjust class names

--- src/components/launchBox/LaunchBox.js
+++ src/components/launchBox/LaunchBox.js
@@ -1,18 +1,19 @@
-import { copyIcon } from '../../assets/icons/icons.js';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './launchBox.scss';
 
 export default function LaunchBox({name, id, image, index, handleCopyToClipboard, className}) {
   return (
-    <div className={`past-launch-elements__elem ${className}`} key={index}>
-      <h3 className='past-launch-elements__name'>{name}</h3>
-      <img className='past-launch-elements__image' src={image} alt={`The patch of the crew ${name}`} />
-      <div className='past-launch-elements__id' data-id={id}>
-        <div className='past-launch-elements__id-text'>
-          <span className='past-launch-elements__id-text-id'>ID: </span>{id}
+    <div className={`latest-launch__list-item ${className}`} key={index}>
+      <h3 className='latest-launch__list-item-name'>{name}</h3>
+      <img className='latest-launch__list-item-image' src={image} alt={`The patch of the crew ${name}`} />
+      <div className='latest-launch__list-item-id' data-id={id}>
+        <div className='latest-launch-elements__id-text'>
+          <span className='latest-launch__list-item-text-id'>ID: </span>{id}
         </div>
-        <button className='past-launch-elements__copy-button' onClick={handleCopyToClipboard}>
+        <button className='latest-launch__list-item-copy' onClick={handleCopyToClipboard}>
           <span className='visually-hidden'>Copy launch ID to clipboard</span>
-          <span className='past-launch-elements__copy-icon'>{copyIcon}</span>
+          <FontAwesomeIcon icon={faCopy} className='latest-launch__list-item-copy-icon'/>
         </button>
       </div>
    </div>
