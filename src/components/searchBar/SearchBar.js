a077d445cea32e377b6eb3feb70ca8d7d3f93747
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-04-15 14:47:19 +0200

    ♻️ Add basic input validation to SearchBar

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,9 +1,34 @@
 import './searchbar.scss';
 
-export default function SearchBar ({searchItems, searchForm, className}) {
+export default function SearchBar ({searchItems, searchForm, setErrorMsg, className}) {
+  const handleSubmit = (event) => {
+    // Prevent form submission by default
+    event.preventDefault();
+
+    const form = searchForm.current;
+    const searchedID =`${`${form['search'].value}`}`;
+
+    // Validate input for empty strings or whitespace
+    if (searchedID === '' || /^\s+$/.test(searchedID)) {
+      setErrorMsg(true);
+      return;
+    }
+
+    // Validate input for alphanumeric characters
+    const alphanumericRegex = /^[a-zA-Z0-9\s]*$/;
+
+    if (!alphanumericRegex.test(searchedID)) {
+      setErrorMsg(true);
+      return;
+    } else {
+      searchItems(event);
+    }
+
+  };
+
   return (
     <div className={`searchbar-container ${className}`}>
-      <form ref={searchForm} className='searchbar'>
+      <form ref={searchForm} className='searchbar' onSubmit={handleSubmit}>
         <label htmlFor='search' className='visually-hidden'>Search launch by ID</label>
         <input
             type='search'
@@ -12,8 +37,9 @@ export default function SearchBar ({searchItems, searchForm, className}) {
             name='search'
             placeholder='Get more info by ID...'
             autoComplete='off'
+            required="required"
           />
-        <button className='button searchbar__button' onClick={searchItems} type='submit'>Search</button>
+        <button className='button searchbar__button' type='submit'>Search</button>
       </form>
     </div>
   )
