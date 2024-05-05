3d5a82e990f0275339d6aed7bf42e301539ace63
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-05-05 11:15:14 +0200

    ♻️ Add LaunchStatus to LaunchBox

--- src/components/launchBox/LaunchBox.js
+++ src/components/launchBox/LaunchBox.js
@@ -1,14 +1,20 @@
+import LaunchStatus from '../launchStatus/LaunchStatus.js';
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './launchBox.scss';
 
 export default function LaunchBox({name, id, success, image, index, handleCopyToClipboard, className}) {
   return (
-    <div className={`latest-launch__list-item ${className}`} key={index} data-success={success}>
-      <h3 className='latest-launch__list-item-name'>{name}</h3>
+    <div className={`latest-launch__list-item ${className}`} key={index}>
+      <div className='latest-launch__list-item-header'>
+        <h3 className='latest-launch__list-item-name'>{name}</h3>
+        <div className='latest-launch__list-item-launch-status-wrapper'>
+          <LaunchStatus launchStatus={success} className={'latest-launch__list-item-launch-status'}/>
+        </div>
+      </div>
       <img className='latest-launch__list-item-image' src={image} alt={`The patch of the crew ${name}`} />
       <div className='latest-launch__list-item-id' data-id={id}>
-        <div className='latest-launch-elements__id-text'>
+        <div className='latest-launch-list-item-id-text'>
           <span className='latest-launch__list-item-text-id'>ID: </span>{id}
         </div>
         <button className='latest-launch__list-item-copy' onClick={handleCopyToClipboard}>
